b8ee9d782e4d6c813f58a05e575446b2a3333d70
derekleeck <ckdh1704@gmail.com>
2023-04-18 23:23:24 +0200

    BASE_URL env

---
+++ src/common/constants/apis-url.ts
@@ -0,0 +1,4 @@
+export const PAV_API = process.env.BASE_URL;
+
+export const OPEN_AI = `${PAV_API}/open-ai`;
+export const MESSAGE = 'message';
--- src/open-ai/open-ai.controller.ts
+++ src/open-ai/open-ai.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { OpenAiService } from './open-ai.service';
 import { GetAiModelAnswer } from './model/get-ai-model-answer';
+import { MESSAGE, OPEN_AI } from 'src/common/constants/apis-url';
 
-@Controller('open-ai')
+@Controller(OPEN_AI)
 export class OpenAiController {
   constructor(private readonly openAiService: OpenAiService) {}
 
-  @Post('/message')
+  @Post(MESSAGE)
   getModelAnswer(@Body() data: GetAiModelAnswer) {
     return this.openAiService.getModelAnswer(data.question);
   }
